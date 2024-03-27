82322663c4a7de75fad077a02bf1b03980ee7308
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-28 02:16:13 +0300

    Model entity created

---
+++ src/rentACarProject/Domain/Entities/Model.cs
@@ -0,0 +1,19 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Model : Entity<Guid>
+{
+    public Guid BrandId { get; set; }
+    public int FuelId { get; set; }
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+    public virtual Brand? Brand { get; set; }
+    public virtual Fuel? Fuel { get; set; }
+}
